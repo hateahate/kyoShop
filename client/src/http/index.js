4c7b0486d37208439be07c2f6edc7ff3ff8af79a
VitaforestDevelopment <95245416+VitaforestDevelopment@users.noreply.github.com>
2022-04-06 20:29:19 +0300

    Добавлен env, прописаны элементы по гайду

--- client/src/http/index.js
+++ client/src/http/index.js
@@ -1,9 +1,20 @@
 import axios from "axios";
 
-const $host  = axios.create({
-
+const $host = axios.create({
+    baseURL: process.env.SERVER_URL
 })
 
-const $authHost  = axios.create({
-
+const $authHost = axios.create({
+    baseURL: process.env.SERVER_URL
 })
+
+const authInterceptor = config => {
+    config.headers.authorization = 'Bearer ${localStorage.getItem('token')}'
+    return config
+}
+
+$authHost.interceptors.request.use(authInterceptor);
+export {
+    $host,
+    $authHost
+}
