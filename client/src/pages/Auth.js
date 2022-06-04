9e8b70d184bfeb2716a8b5ceaabf43ca627a1450
VitaforestDevelopment <95245416+VitaforestDevelopment@users.noreply.github.com>
2022-06-04 12:02:10 +0300

    Реализация авторизации (основные компоненты)

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,11 +1,32 @@
-import { Container } from '@mui/material'
-import React from 'react'
-const Auth = () => {
+import React, { useContext, useState } from 'react';
+import { NavLink, useLocation } from "react-router-dom";
+import { LOGIN_ROUTE, REGISTRATION_ROUTE, SHOP_ROUTE } from "../utils/consts";
+import { login, registration } from "../http/userAPI";
+import { observer } from "mobx-react-lite";
+import { Context } from "../index";
+
+const Auth = observer(() => {
+
+  const { user } = useContext(Context)
+  const location = useLocation()
+  const isLogin = location.pathname === LOGIN_ROUTE
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const click = async () => {
+    let userData;
+    if (isLogin) {
+      userData = await login(email, password);
+    }
+    else {
+      userData = await registration(email, password);
+    }
+    user.setUser(user);
+    user.setIsAuth(true);
+  }
   return (
-    <Container>
-      <h2>Auth</h2>
-    </Container>
+    <div>Hello</div>
   )
-}
+})
 
 export default Auth
