d4b58a6fe93d6661d67bd04316f1e8bb9838fe47
Konstantin Bytsai <k.bytsay@gmail.com>
2022-06-05 01:06:14 +0300

    popups

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,12 @@ import './App.css'
 import { BrowserRouter } from 'react-router-dom'
 import AppRouter from './components/AppRouter'
 import NavBar from './components/NavBar'
+import Header from './components/VitaforestUI/Interface/Header/Header'
 
 const App = () => {
   return (
     <BrowserRouter>
-      <NavBar></NavBar>
+      <Header />
       <AppRouter />
     </BrowserRouter>
   )
--- client/src/components/VitaforestUI/Interface/Header/subcomponents/HeaderMenu.js
+++ client/src/components/VitaforestUI/Interface/Header/subcomponents/HeaderMenu.js
@@ -13,20 +13,46 @@ const BurgerButton = styled.button`
   border: none;
   margin-right: 15px;
 `
+
 const MenuContainer = styled.div`
   width: calc(100vw - 36px);
   margin: 0 auto;
+  position: relative;
   .navigation-menu {
-    display: none;
     position: absolute;
-    left: 0;
     top: 0;
     background-color: #fff;
     width: 90vw;
     height: 100vh;
-    z-index: 2;
+    z-index: 3;
+    left: -100vw;
+    -webkit-transition: left 0.5s;
+    transition: left 0.5s;
+    .close {
+      position: absolute;
+      right: 10px;
+      top: 10px;
+      border: none;
+      background: none;
+    }
   }
-  .expanded {
+  .navigation-menu.expanded {
+    display: block;
+    left: -18px;
+    -webkit-transition: left 0.5s;
+    transition: left 0.5s;
+  }
+  .jija {
+    z-index: 2;
+    position: absolute;
+    width: 100vw;
+    height: 100vh;
+    background: #000;
+    opacity: 0.7;
+    display: none;
+    left: -18px;
+  }
+  .jija.shown {
     display: block;
   }
 `
@@ -34,6 +60,10 @@ const MenuWrapper = styled.div`
   display: flex;
   flex-wrap: nowrap;
   align-items: center;
+  padding-top: 11px;
+  .logo-container.expanded {
+    display: none;
+  }
 `
 
 const SearchShowButton = styled.button`
@@ -53,12 +83,51 @@ const MobileCartButton = styled.button`
 const MobileLogo = styled.div`
   margin-right: auto;
 `
-
+const SearchForm = styled.div`
+  .search-container {
+    position: absolute;
+    display: flex;
+    -webkit-transition: top 0.5s;
+    top: -80px;
+    transition: top 0.5s;
+    width: 100vw;
+    background-color: #fff;
+    z-index: 3;
+    left: -18px;
+    box-sizing: border-box;
+    padding: 10px 20px;
+  }
+  .expanded {
+    -webkit-transition: top 0.5s;
+    transition: top 0.5s;
+    top: 0px;
+  }
+  .search-button {
+    border: none;
+    background-color: #fff;
+    padding-top: 5px;
+    vertical-align: baseline;
+  }
+  .search-input {
+    width: calc(100vw - 68px);
+    display: inline-block;
+    box-shadow: 0px 1px 0px #eaebec;
+    border: none;
+  }
+`
 const HeaderMenu = () => {
   const [isNavExpanded, setIsNavExpanded] = useState(false)
+  const [isSearchExpanded, setIsSearchExpanded] = useState(false)
 
   return (
     <MenuContainer>
+      <div
+        className={isSearchExpanded || isNavExpanded ? 'jija shown' : 'jija'}
+        onClick={() => {
+          setIsNavExpanded(false)
+          setIsSearchExpanded(false)
+        }}
+      ></div>
       <MenuWrapper>
         <BurgerButton
           onClick={() => {
@@ -67,12 +136,34 @@ const HeaderMenu = () => {
         >
           <BurgerIcon />
         </BurgerButton>
+
         <MobileLogo>
           <Link to="/">
             <LogoMobileIcon />
           </Link>
         </MobileLogo>
-        <SearchShowButton>
+        <SearchForm>
+          <div
+            className={
+              isSearchExpanded
+                ? 'search-container expanded'
+                : 'search-container'
+            }
+          >
+            <input
+              className="search-input"
+              placeholder="Search entire store here..."
+            />
+            <button className="search-button">
+              <MobileSearchIcon />
+            </button>
+          </div>
+        </SearchForm>
+        <SearchShowButton
+          onClick={() => {
+            setIsSearchExpanded(!isSearchExpanded)
+          }}
+        >
           <MobileSearchIcon />
         </SearchShowButton>
         <MobileUserButton>
