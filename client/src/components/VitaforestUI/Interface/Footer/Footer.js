02d7812eccb34cedea461784ab8616acbad72c3b
Konstantin Bytsai <k.bytsay@gmail.com>
2022-06-13 18:49:33 +0300

    Дропдауны футера мобилки

--- client/src/components/VitaforestUI/Interface/Footer/Footer.js
+++ client/src/components/VitaforestUI/Interface/Footer/Footer.js
@@ -21,6 +21,15 @@ const ContactsText = styled.p`
   opacity: 0.5;
   margin-top: 27px;
 `
+
+const FooterNav = styled.div``
+
+const FooterNavWrapper = styled.div`
+  .nav-expanded {
+    display: block;
+  }
+`
+
 const NavTitle = styled.h3`
   font-family: 'Proxima Nova';
   font-style: normal;
@@ -40,10 +49,30 @@ const NavTitle = styled.h3`
     top: 2px;
   }
 `
-const FooterNavList = styled.div``
-const FooterNav = styled.div``
+const NavList = styled.ul`
+  list-style: none;
+  padding: 0;
+  display: none;
+  a {
+    text-decoration: none;
+    font-family: 'Proxima Nova';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 158%;
+    text-decoration-line: underline;
+    color: #ffffff;
+    opacity: 0.9;
+  }
+`
 
+const NavLink = styled.li`
+  padding-left: 20px;
+`
 const Footer = () => {
+  const [isCompanyExpanded, setIsCompanyExpanded] = useState(false)
+  const [isCustomerExpanded, setIsCustomerExpanded] = useState(false)
+  const [isInspiredExpanded, setIsInspiredExpanded] = useState(false)
   return (
     <FooterContainer>
       <FooterInner>
@@ -65,15 +94,72 @@ const Footer = () => {
           </ContactsText>
         </Contacts>
         <FooterNav>
-          <FooterNavList>
-            <NavTitle>Company information</NavTitle>
-          </FooterNavList>
-          <FooterNavList>
-            <NavTitle>Customer service</NavTitle>
-          </FooterNavList>
-          <FooterNavList>
-            <NavTitle>Inspired by</NavTitle>
-          </FooterNavList>
+          <FooterNavWrapper>
+            <NavTitle
+              onClick={() => {
+                setIsCompanyExpanded(!isCompanyExpanded)
+              }}
+            >
+              Company information
+            </NavTitle>
+            <NavList
+              className={isCompanyExpanded ? 'nav-expanded' : 'unexpanded'}
+            >
+              <NavLink>
+                <Link to="/">About us</Link>
+              </NavLink>
+              <NavLink>
+                <Link to="/">Company news</Link>
+              </NavLink>
+              <NavLink>
+                <Link to="/">Production</Link>
+              </NavLink>
+            </NavList>
+          </FooterNavWrapper>
+          <FooterNavWrapper>
+            <NavTitle
+              onClick={() => {
+                setIsCustomerExpanded(!isCustomerExpanded)
+              }}
+            >
+              Customer service
+            </NavTitle>
+            <NavList
+              className={isCustomerExpanded ? 'nav-expanded' : 'unexpanded'}
+            >
+              <NavLink>
+                <Link to="/">Your account</Link>
+              </NavLink>
+              <NavLink>
+                <Link to="/">Contact us</Link>
+              </NavLink>
+              <NavLink>
+                <Link to="/">FAQ</Link>
+              </NavLink>
+            </NavList>
+          </FooterNavWrapper>
+          <FooterNavWrapper>
+            <NavTitle
+              onClick={() => {
+                setIsInspiredExpanded(!isInspiredExpanded)
+              }}
+            >
+              Inspired by
+            </NavTitle>
+            <NavList
+              className={isInspiredExpanded ? 'nav-expanded' : 'unexpanded'}
+            >
+              <NavLink>
+                <Link to="/">Industry news</Link>
+              </NavLink>
+              <NavLink>
+                <Link to="/">Trends</Link>
+              </NavLink>
+              <NavLink>
+                <Link to="/">Products in stock</Link>
+              </NavLink>
+            </NavList>
+          </FooterNavWrapper>
         </FooterNav>
       </FooterInner>
     </FooterContainer>
