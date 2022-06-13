9ad8df30ee509479fa42770218d51ab85ac86343
Konstantin Bytsai <k.bytsay@gmail.com>
2022-06-13 14:59:59 +0300

    Мобильная навигация хедера

--- client/src/components/VitaforestUI/Interface/Header/subcomponents/Navigation.js
+++ client/src/components/VitaforestUI/Interface/Header/subcomponents/Navigation.js
@@ -3,24 +3,73 @@ import styled from 'styled-components'
 
 const Nav = styled.nav`
   background-color: #fff;
+  margin-top: 50px;
 `
 const MainList = styled.div`
   list-style: none;
 `
 const SubList = styled.ul`
+  margin: 0px;
   list-style: none;
+  padding: 20px 18px;
+  background: #f7f8f9;
+  .first-sub-item {
+    padding-top: 0px;
+  }
 `
 
-const MainItem = styled.p``
-const SubItem = styled.li``
-const SubItemMain = styled.li``
+const MainItem = styled.p`
+  font-family: 'Proxima Nova';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 22px;
+  display: flex;
+  align-items: center;
+  box-shadow: 0px 1px 0px #eaebec;
+  margin: 0;
+  padding: 9px 18px;
+  &:hover,
+  &:focus,
+  &:active {
+    color: #40bf6a;
+  }
+`
+const SubItem = styled.li`
+  padding-top: 15px;
+  font-family: 'Proxima Nova';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 17px;
+  color: #303236;
+  &:hover,
+  &:focus,
+  &:active {
+    color: #40bf6a;
+  }
+`
+const SubItemMain = styled.li`
+  font-weight: 600;
+  font-family: 'Proxima Nova';
+  font-size: 16px;
+  line-height: 19px;
+  text-decoration-line: underline;
+  padding-top: 15px;
+  color: #303236;
+  &:hover,
+  &:focus,
+  &:active {
+    color: #40bf6a;
+  }
+`
 const Navigation = () => {
   return (
     <Nav>
       <MainList>
         <MainItem>Products</MainItem>
         <SubList>
-          <SubItemMain>Extracts</SubItemMain>
+          <SubItemMain className="first-sub-item">Extracts</SubItemMain>
           <SubItem>Herbal extracts</SubItem>
           <SubItem>Mushroom extracts</SubItem>
           <SubItem>Fruit extracts</SubItem>
@@ -31,7 +80,7 @@ const Navigation = () => {
         </SubList>
         <MainItem>Company</MainItem>
         <SubList>
-          <SubItem>About us</SubItem>
+          <SubItem className="first-sub-item">About us</SubItem>
           <SubItem>Production</SubItem>
           <SubItem>FAQ</SubItem>
         </SubList>
