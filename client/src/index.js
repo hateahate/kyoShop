b2f8e20c9423406150100af99b96f2cc7b4dd087
Konstantin Bytsai <k.bytsay@gmail.com>
2022-06-13 12:44:24 +0300

    Добавлен сброс стилей браузеров(отступы и т.д)

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,19 @@
 import React, { createContext } from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import UserStore from "./store/UserStore";
-
+import UserStore from './store/UserStore'
+import { Normalize } from 'styled-normalize'
 export const Context = createContext(null)
 console.log(process.env.REACT_APP_API_URL)
 
 ReactDOM.render(
-    <Context.Provider value={{
-        user: new UserStore()
-    }}>
-
-        <App />
-    </Context.Provider>,
-    document.getElementById('root'))
+  <Context.Provider
+    value={{
+      user: new UserStore(),
+    }}
+  >
+    <Normalize />
+    <App />
+  </Context.Provider>,
+  document.getElementById('root'),
+)
