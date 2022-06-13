589e980acb36a7c9a3ac73d3abefe520233c8458
VitaforestDevelopment <95245416+VitaforestDevelopment@users.noreply.github.com>
2022-06-13 15:52:46 +0300

    Реализация автосоздания ЧПУ на стороне сервера

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -5,11 +5,14 @@ const ApiError = require('../error/ApiError');
 class PostController {
     async create(req, res, next) {
         try {
-            const { title, description, link, postCategoryId } = req.body; // Получаем данные о создаваемом товаре из GET-запроса
+            let { title, description, link, postCategoryId } = req.body; // Получаем данные о создаваемом товаре из GET-запроса
             if (req.files) {
                 const { img } = req.files; // Получаем изображение
                 let fileName = uuid.v4() + ".jpg"; // Генерируем уникальное название изображению
                 img.mv(path.resolve(__dirname, '..', 'static', fileName)); // Загружаемые изображения кидаем в директорию static на сервере и даём уникальное имя
+                let mask = / /g;
+                let prelink = title.replace(mask, '-');
+                link = prelink;
                 const post = await Post.create({ title, description, img: fileName, link, postCategoryId });
                 return res.json(post);
             }
