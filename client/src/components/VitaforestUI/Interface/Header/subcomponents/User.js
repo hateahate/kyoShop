c92e00d4d36f71375d40f3c2e41249b0e69efb41
kbytsai <91729673+kbytsai@users.noreply.github.com>
2022-06-15 16:29:55 +0300

    Добавлен компонент юзера в хедер
    Добавлен компонент юзера в хедер

---
+++ client/src/components/VitaforestUI/Interface/Header/subcomponents/User.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+const UserContainer = styled.div``
+
+const UserMenu = (props) => {
+  return <UserContainer>User</UserContainer>
+}
+
+export default User
