65eaf8043528184a542ec35e84f6e068dabd899a
hateahate <stepanpavlenko@icloud.com>
2022-08-02 13:05:05 +0300

    Реализовано клиентское api для постов

---
+++ client/src/api/postAPI.js
@@ -0,0 +1,32 @@
+import { $authHost, $host } from ".";
+import jwt_decode from "jwt-decode";
+
+export const removePost = async (id) => {
+    const { data } = await $host.post('api/product/remove', id)
+    return data
+}
+
+
+export const createPost = async (post) => {
+    const { data } = await $authHost.post('api/post', post)
+    return data
+}
+
+export const updatePost = async (product) => {
+    const { data } = await $host.post('api/post/update', post)
+    return data
+}
+
+export const fetchPosts = async (page, limit = 5) => {
+    const { data } = await $host.get('api/post', {
+        params: {
+            page, limit
+        }
+    })
+    return data
+}
+
+export const fetchOnePost = async (id) => {
+    const { data } = await $host.get(`api/product/${id}`)
+    return data
+}
