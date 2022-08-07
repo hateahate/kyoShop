9c93ea72da5eff5583b6a0ddc8b185df38debfbb
VitaforestDevelopment <95245416+VitaforestDevelopment@users.noreply.github.com>
2022-08-07 16:21:06 +0300

    В дэшборде тестирую рендер html из json-форматированного тела поста

--- client/src/components/Admin/Admin.js
+++ client/src/components/Admin/Admin.js
@@ -1,12 +1,22 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import AdminUI from './Ui/AdminUI';
+import { fetchOnePost, htmlPostBody } from '../../api/postAPI';
 
 const Admin = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState(null);
+    fetchOnePost(2).then(data => {
+        setTitle(data.title)
+        setBody(htmlPostBody(data.description))
+    })
     return (
         <AdminUI>
             <h1>Dashboard</h1>
-            <p>Здесь пока ничего нет, но сущность отвечающая за разного рода уведомления в бэке существует. Нужно дописать API для работы с ним как на бэке так и на фронте.</p>
+            <h2>Render post test</h2>
+            <h3>Title: {title}</h3>
+            <div dangerouslySetInnerHTML={{ __html: body }}>
+            </div>
         </AdminUI>
     )
 }
