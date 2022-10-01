fdfc139337c70f041357bdddaf0a42545ceb641c
VitaforestDevelopment <95245416+VitaforestDevelopment@users.noreply.github.com>
2022-10-01 10:39:17 +0300

    Реализована цепочка загрузки данных с исключениями для EditUser

--- client/src/components/Admin/Pages/EditUser/EditUser.js
+++ client/src/components/Admin/Pages/EditUser/EditUser.js
@@ -26,17 +26,8 @@ const EditUser = () => {
     const [email, setEmail] = useState('')
     const [role, setRole] = useState('')
     const [approved, setApproved] = useState('')
-
-    // Load Data
-    useEffect(() => {
-        getUserById(id).then((data) => {
-            setFirstName(data.first_name)
-            setLastName(data.last_name)
-            setEmail(data.email)
-            setRole(data.role)
-            setApproved(data.approved)
-        })
-    }, [])
+    const [isLoaded, setIsLoaded] = useState(false)
+    const [error, setError] = useState()
 
     // Save changes
     const UserUpdate = () => {
@@ -57,58 +48,88 @@ const EditUser = () => {
         });
     }
 
-    return (
-        <AdminUI>
-            <NotificationContainer />
-            <Form>
-                <Container>
-                    <Card>
+    // Load Data
+    useEffect(() => {
+        getUserById(id).then((data) => {
+            setFirstName(data.first_name)
+            setLastName(data.last_name)
+            setEmail(data.email)
+            setRole(data.role)
+            setApproved(data.approved)
+            setIsLoaded(true)
+        }, (error) => {
+            setIsLoaded(false)
+            setError(error)
+        })
+    }, [])
+    if (error) {
+        return (
+            <div>
+                {NotificationManager.error(`${error.message}`, 'Error')}
+                <NotificationContainer />
+            </div>
+        )
+    }
+    else if (!isLoaded) {
+        return (
+            <h1>Loading...</h1>
+        )
+    }
+    else {
+        return (
+            <AdminUI>
+                <NotificationContainer />
+                <h1>Edit user {email}</h1>
+                <Form>
+                    <Container>
                         <Card>
-                            <Card.Header>
-                                First name
-                            </Card.Header>
-                            <Card.Body>
-                                <Form.Control aria-label="large" value={firstName} onChange={e => setFirstName(String(e.target.value))} placeholder='First name' type='text' />
-                            </Card.Body>
+                            <Card>
+                                <Card.Header>
+                                    First name
+                                </Card.Header>
+                                <Card.Body>
+                                    <Form.Control aria-label="large" value={firstName} onChange={e => setFirstName(String(e.target.value))} placeholder='First name' type='text' />
+                                </Card.Body>
+                            </Card>
+                            <Card>
+                                <Card.Header>
+                                    Last name
+                                </Card.Header>
+                                <Card.Body>
+                                    <Form.Control aria-label="large" value={lastName} onChange={e => setLastName(String(e.target.value))} placeholder='First name' type='text' />
+                                </Card.Body>
+                            </Card>
+                            <Card>
+                                <Card.Header>
+                                    Email
+                                </Card.Header>
+                                <Card.Body>
+                                    <Form.Control aria-label="large" value={email} onChange={e => setEmail(String(e.target.value))} placeholder='user@dejikyo.com' type='email' />
+                                </Card.Body>
+                            </Card>
+                            <Card>
+                                <Card.Header>
+                                    Role
+                                </Card.Header>
+                                <Card.Body>
+                                    <Form.Control aria-label="large" value={role} onChange={e => setRole(String(e.target.value))} placeholder='role' type='text' />
+                                </Card.Body>
+                            </Card>
+                            <Card>
+                                <Card.Header>
+                                    Approved
+                                </Card.Header>
+                                <Card.Body>
+                                    <Form.Control aria-label="large" value={approved} onChange={e => setApproved(String(e.target.value))} placeholder='' type='text' />
+                                </Card.Body>
+                            </Card>
+                            <Card.Footer><Button variant="success" onClick={UserUpdate}>Update</Button></Card.Footer>
                         </Card>
-                        <Card>
-                            <Card.Header>
-                                Last name
-                            </Card.Header>
-                            <Card.Body>
-                                <Form.Control aria-label="large" value={lastName} onChange={e => setLastName(String(e.target.value))} placeholder='First name' type='text' />
-                            </Card.Body>
-                        </Card>
-                        <Card>
-                            <Card.Header>
-                                Email
-                            </Card.Header>
-                            <Card.Body>
-                                <Form.Control aria-label="large" value={email} onChange={e => setEmail(String(e.target.value))} placeholder='user@dejikyo.com' type='email' />
-                            </Card.Body>
-                        </Card>
-                        <Card>
-                            <Card.Header>
-                                Role
-                            </Card.Header>
-                            <Card.Body>
-                                <Form.Control aria-label="large" value={role} onChange={e => setRole(String(e.target.value))} placeholder='role' type='text' />
-                            </Card.Body>
-                        </Card>
-                        <Card>
-                            <Card.Header>
-                                Approved
-                            </Card.Header>
-                            <Card.Body>
-                                <Form.Control aria-label="large" value={approved} onChange={e => setApproved(String(e.target.value))} placeholder='' type='text' />
-                            </Card.Body>
-                        </Card>
-                        <Card.Footer><Button variant="success" onClick={UserUpdate}>Update</Button></Card.Footer>
-                    </Card>
-                </Container>
-            </Form>
-        </AdminUI>
-    )
+                    </Container>
+                </Form>
+            </AdminUI>
+        )
+    }
 }
 
 export default EditUser
