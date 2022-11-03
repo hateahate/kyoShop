380ffcb02d6e5ca7ce2f4c6a81b362b19b214a7d
kbytsai <91729673+kbytsai@users.noreply.github.com>
2022-11-03 15:33:17 +0300

    Компонент промо основные стили

--- client/src/components/VitaforestUI/Interface/Header/subcomponents/Promo.js
+++ client/src/components/VitaforestUI/Interface/Header/subcomponents/Promo.js
@@ -1,5 +1,20 @@
 import React from "react";
+import styled from "styled-components";
 
-export default function Promo() {
-  return <div>Promo</div>;
+const Container = styled.div`
+  box-sizing: border-box;
+  padding: 30px;
+  background-image: ${(props) => props.image};
+`;
+
+const Title = styled.h4`
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 19px;
+  color: #ffffff;
+  margin-bottom: 8px;
+  margin-top: auto;
+`;
+export default function Promo(props) {
+  return <Container></Container>;
 }
