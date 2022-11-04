673b6d0375f70bf67c67c46033fdb9570b8837a2
VitaforestDevelopment <95245416+VitaforestDevelopment@users.noreply.github.com>
2022-11-04 17:06:28 +0300

    Первая попытка изменения количества товаров на стоке при создании заказа

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,12 +1,42 @@
-const { Order } = require('../models/models');
+const { Order, Product } = require('../models/models');
 const ApiError = require('../error/ApiError');
+const { Sequelize } = require('../db');
+const { Op } = Sequelize;
 
 class OrderController {
     async create(req, res, next) {
         try {
             let { status, items, userId } = req.body;
+            let jsonItems = JSON.parse(items);
+            let itemsId = [];
+            let descQty = [];
+            // parse items id
+            jsonItems.map((item) => {
+                itemsId.push(item.id)
+            })
+            // fetch all products with ids
+            const products = await Product.findAll({ where: { id: { [Op.in]: itemsId } } });
+            // convert to json from sequelize instance
+            let productsList = JSON.stringify(products)
+            // reparse list from string to object
+            productsList = JSON.parse(productsList)
+            console.log(productsList)
+            await productsList.map((item) => {
+                let listItemId = item.id;
+                jsonItems.find((elem) => {
+                    if (elem.id === listItemId) {
+                        item.stock = item.stock - elem.qty;
+                        Product.update({ stock: item.stock }, { where: { id: item.id } })
+                        console.log(item.stock)
+                    }
+
+                })
+                console.log(productsList)
+            })
+            console.log(qtysfromlist)
+            // create order
             const order = await Order.create({ status, items, userId });
-            return res.json(order);
+            return res.json('yep');
         } catch (e) {
             next(ApiError.badRequest(e.message));
         }
